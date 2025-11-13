e4bec826b99aeffd3f8e7054ea037b8e34d7725b
laboratoriohuygens-dev <laboratoriohuygens@gmail.com>
2025-11-13 10:58:54 -0300

    Create derivada.py

---
+++ Unidade 3/derivada.py
@@ -0,0 +1,54 @@
+def Regra_Extremidade_Esquerda (a = 0, b = 0, c= 0, d = -2, e = 0, f = 2,
+                                x_i = -2, x_f = 2, passo = 0.25,
+                                plot = False):
+  intervalo = []
+  for i in range(int((x_f - x_i)/passo)):
+    intervalo.append(x_i + i*passo)
+
+  fxi = []
+  ci_fxi = []
+  for i in range(len(intervalo)):
+    x = intervalo[i]
+    f_x_i = a*x**5 + b*x**4 + c*x**3 + d*x**2 + e*x + f
+    ci_fxi.append(f_x_i*passo)
+    fxi.append(f_x_i)
+
+  somatoria = sum(ci_fxi)
+
+  integral_xi = (a/6)*x_i**6 + (b/5)*x_i**5 + (c/4)*x_i**4 + (d/3)*x_i**3 + (e/2)*x_i**2 + f*x_i
+  integral_xf = (a/6)*x_f**6 + (b/5)*x_f**5 + (c/4)*x_f**4 + (d/3)*x_f**3 + (e/2)*x_f**2 + f*x_f
+  integral_analitica = integral_xf - integral_xi
+
+  dicionario = {
+      'Intervalo': intervalo,
+      'f(x)': fxi,
+      'Integral Numérica': somatoria,
+      'Integral Analítica': integral_analitica}
+
+  if plot == True:
+    # Plotando
+    plt.figure(figsize=(10,6))
+    plt.plot(intervalo, fxi, 'b',
+             label = rf'$f (x) = {a}x^5 + {b}x^4 + {c}x^3 + {d}x^2 + {e}x + {f}$')  # curva da função
+
+    # Desenhando os retângulos
+    for i in range(len(intervalo)):
+        x0 = intervalo[i]
+        plt.bar(x0, fxi[i], width=passo, align='edge', color='orange', edgecolor='black', alpha=0.5)
+
+    plt.text(0.3, 0.15,
+             rf'$\int_{{{x_i}}}^{{{x_f}}} f(x)\,dx=\int_{{{x_i}}}^{{{x_f}}} {a}x^5 + {b}x^4 + {c}x^3 + {d}x^2 + {e}x + {f}\,dx = {somatoria:.2f}$',
+             transform=plt.gca().transAxes,
+             fontsize=12,
+             verticalalignment='top',
+             bbox=dict(boxstyle="round,pad=0.3", facecolor='white', alpha=0.7))
+
+    # Detalhes do gráfico
+    plt.title('Integração Numérica - Regra da Extremidade Esquerda')
+    plt.xlabel(r'$x$')
+    plt.ylabel(r'$f(x)$')
+    plt.legend()
+    plt.savefig('integracao_extremidade_esquerda.png', dpi = 1200)
+    plt.show()
+
+  return dicionario
